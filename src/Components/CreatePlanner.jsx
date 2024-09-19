bb8474a0ebff6dcc779711f4078e5236f0431bc4
guswlda <guswl71945@gmail.com>
2024-09-19 10:38:56 +0900

    disable update

--- src/Components/CreatePlanner.jsx
+++ src/Components/CreatePlanner.jsx
@@ -183,7 +183,6 @@ const Createplanner = () => {
                         value={startDate}
                         onChange={handleDateChange}
                         className="Logo_text bg-white w-full rounded-md p-3 border border-slate-300"
-                        disabled
                       />
                       <input
                         type="date"
@@ -192,7 +191,6 @@ const Createplanner = () => {
                         value={endDate}
                         onChange={handleDateChange}
                         className="Logo_text bg-white w-full rounded-md p-3 border border-slate-300"
-                        disabled
                       />
                     </div>
                   </div>
